76c2ee485af8cbfabb66410810deda81886fc257
Erico Weber <ericoweber@avato.com.br>
2021-03-02 22:27:56 -0300

    criacao do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+    {{-- <title></title> --}}
+    <!-- {{-- <link rel="icon" href="{!! asset('assets/gcm_ico.ico') !!}"/>  --}} -->
+</head>
+@extends('adminlte::page')
+@section('title', 'INRI')
+
+@section('content_header')
+
+<h1>Olá {{Auth::user()->name}}, bem-vindo ao Dashboard LabEnsaios</h1>
+
+
+
+@stop
+@section('content')
+
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                background-image: url('/assets/wallpaper.jpg');
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                /*background-position: center; */
+                color: #050E59;
+                font-family: 'Nunito', sans-serif;
+
+                font-weight: 200;
+                background-size:100%;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: left;
+                display: flex;
+                justify-content: center;
+                top: 25px;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                /*position: absolute;*/
+                /*right: 10px;*/
+                /*top: 1px;*/
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 64px;
+            }
+
+            .links > a {
+                color: #050E59;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                /* margin-bottom: 1px; */
+                margin-top: 1%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title m-b-md">
+                    <img src="/assets/INRICores.png" width='20%'/>
+                </div>
+                 @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Entrar</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">REGISTRAR</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            </div>
+        </div>
+    </body>
+</html>
